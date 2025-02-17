e8daffa869fd789f61b63c6f04b9b69b4f2fa882
Ang91 <angiepao2420@gmail.com>
2025-02-16 23:30:38 -0500

    estilos actualizados

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  alert(`¡Gracias por tu mensaje, ${name}!\nPronto nos pondremos en contacto contigo.`);
-});
